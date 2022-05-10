3ff314773433b675a0f7d5108242941b430681bb
Oeyni <oeyni.exe@gmail.com>
2022-05-10 21:49:51 +0200

    db conn, select max id

--- main.java
+++ main.java
@@ -1,21 +1,26 @@
 
 public class main {
+    //static nur zum testen
+    static int iFrage_ID;
+    static String sFrage_Text ="Frage";
+    static String sAntwort_1 = "A 1";
+    static String sAntwort_2 = "A 2";
+    static String sAntwort_3 = "A 3";
+    static String sAntwort_4 = "A 4";
 
-    String sFrage_Text ="Frage";
-    String sAntwort_1 = "A 1";
-    String sAntwort_2 = "A 2";
-    String sAntwort_3 = "A 3";
-    String sAntwort_4 = "A 4";
-
-    Integer iRichtige_Antwort;
+    int iRichtige_Antwort;
 
     //GUI Starten
     public static void main(String[] args)
     {
-        mysql.connect();
-        gui.main(args);
+
+        mysql.maxID();
+        System.out.println(iFrage_ID);
+        mysql.select();
+        //gui.main(args);
 
     }
+
 
 
 
